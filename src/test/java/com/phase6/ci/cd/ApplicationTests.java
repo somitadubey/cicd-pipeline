4827928ff56aa08c717a1bcbbae2e3280e311a65
dubeysomita <duber.somita@gmail.com>
2022-04-16 09:14:40 +0000

    fisrt commit

---
+++ src/test/java/com/phase6/ci/cd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phase6.ci.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
